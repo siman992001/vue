1c56e6f17612881dc74b0a5a6b99b11f575de52c
Ramon A Lewis <lewis.a.ramon@gmail.com>
2022-06-17 18:02:54 -0400

    v-html directive

--- src/components/CountComp.vue
+++ src/components/CountComp.vue
@@ -2,9 +2,10 @@
 import { onMounted, onUnmounted, ref } from "vue";
 
 const count = ref(0);
+const msg = '<span style="color: red">This should be red</span>';
 
 const increment = () => {
-  count.value ++;
+  count.value++;
 };
 
 onMounted(() => {
@@ -17,5 +18,7 @@ onUnmounted(() => {
 </script> 
 
 <template>
+  <p>Using text interpolation directive {{msg}}</p>
+  <p>Using v-html directive <span v-html="msg"></span></p>
   <button @click="increment">Count is {{ count }}</button>
 </template>
