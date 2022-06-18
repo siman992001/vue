e4f02265a1cec41af2c1f7424e321515b29666e0
Ramon A Lewis <lewis.a.ramon@gmail.com>
2022-06-18 07:30:37 -0400

    disabled

--- src/components/CountComp.vue
+++ src/components/CountComp.vue
@@ -3,6 +3,11 @@ import { onMounted, onUnmounted, ref } from "vue";
 
 const count = ref(0);
 const msg = '<span style="color: red">This should be red</span>';
+const my_id = 5;
+
+const isButtonDisabled = () =>{
+  return true;
+}
 
 const increment = () => {
   count.value++;
@@ -20,5 +25,7 @@ onUnmounted(() => {
 <template>
   <p>Using text interpolation directive {{msg}}</p>
   <p>Using v-html directive <span v-html="msg"></span></p>
-  <button @click="increment">Count is {{ count }}</button>
+  <button :disabled="isButtonDisabled" @click="increment">Count is {{ count }}</button>
+  <div :id="my_id">My ID</div>
+  <div>{{"my name is ramon lewis".split(" ")[3]}}</div>
 </template>
