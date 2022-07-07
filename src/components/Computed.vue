f535e7cf667f23b966d95839e6f9ea54b963e129
Ramon A Lewis <lewis.a.ramon@gmail.com>
2022-07-07 07:55:06 -0400

    computed

--- src/components/Computed.vue
+++ src/components/Computed.vue
@@ -5,12 +5,33 @@ const person = reactive({
   cars: ["BMW", "Tesla", "Honda", "Volvo", "Audi"],
 });
 
-const owns_cars = computed(()=>{
-    return person.cars.length > 0 ? "Owns " + person.cars.length + " cars" : "No Cars"
-})
+const owns_cars = computed(() => {
+  return person.cars.length > 0
+    ? "Owns " + person.cars.length + " cars"
+    : "No Cars";
+});
+
+const my_cars = computed({
+  get() {
+    return person.cars.length > 0
+      ? "Owns " + person.cars.length + " cars"
+      : "No Cars";
+  },
+  set(value) {
+    person.cars.push(value);
+  },
+});
+
+const changeMe = () => {
+  my_cars.value = "Porsche";
+};
 </script>
 
 <template>
   <h1>{{ person.name }}</h1>
-  <h2>{{owns_cars}}</h2>
+  <h2>Computed property:</h2>
+  <h4>{{ owns_cars }}</h4>
+  <h2>Method:</h2>
+  <h4>{{ my_cars }}</h4>
+  <button @click="changeMe">Click to change</button>
 </template>
